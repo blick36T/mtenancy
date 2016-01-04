f4df39d0b4ca439cadb1b5829516a689e81adf68
Sruthi <sruthikrishnamurthy1993@gmail.com>
2016-01-04 17:49:16 +0530

    Update app/Survey.php

--- app/Survey.php
+++ app/Survey.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-  protected $fillable = ['email','name', 'color', 'pet'];
+  protected $fillable = ['email','name', 'color', 'pet','nickname','shape','pet_count','opt_in','how_hear'];
   protected $guarded = ['id', 'created_at', 'updated_at'];
 }
